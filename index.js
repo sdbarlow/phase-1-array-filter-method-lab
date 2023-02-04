0a1c955b2783d333e8468f73dc978a10ebc77701
Seth Barlow <sethdbarlow@gmail.com>
2023-02-04 13:30:16 -0700

    all tests pass

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+
+
+function findMatching(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.toLowerCase() === sought.toLowerCase()
+    );
+  }
+
+
+function fuzzyMatch(source, sought) {
+    return source.filter(
+      (possibleMatch) => possibleMatch.charAt(0).toLowerCase() === sought.charAt(0).toLowerCase()
+    );
+
+}
+
+function matchName(source, str){
+    return source.filter((data) => data.name.toLowerCase() === str.toLowerCase()
+    );
+
+}
+
+
